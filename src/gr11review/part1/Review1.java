0e9e68d8c64e8a9ae116b47a36baf607bbeaa3d4
AidenLum <aiden.lum25@ycdsbk12.ca>
2024-09-12 17:02:14 -0400

    WIP Created variables and buffered reader for #1

--- src/gr11review/part1/Review1.java
+++ src/gr11review/part1/Review1.java
@@ -1,1 +1,25 @@
 package gr11review.part1;
+
+// Program to calculate the day of year with month and day entered
+
+import java.io.*; // import code
+
+public class Review1{
+    public static void main(String[] args) throws IOException{
+        
+        BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+
+        // Prompt user to input month and day number
+        System.out.print("Enter the month number: ");
+        int month = Integer.parseInt(keyboard.readLine());
+
+        System.out.print("Enter day number");
+        int day = Integer.parseInt(keyboard.readLine());
+
+        // Proccess the how many days are in each month and calculate the day of the year number
+        while ((month - 1)>= 0){
+
+        }
+        
+    }
+}
--- src/gr11review/part1/Review2.java
+++ src/gr11review/part1/Review2.java
@@ -1,1 +1,13 @@
 package gr11review.part1;
+
+// Program to prompt user to choose a joke catergory and output corresponding joke
+
+public class Review2{
+    public static void main(String[] args) {
+        
+
+
+
+
+    }
+}
