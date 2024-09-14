bda104160ddfdd96735ba2e4e01aca2055766c67
AidenLum <aiden.lum25@ycdsbk12.ca>
2024-09-13 21:43:34 -0400

    WIP adjusted variable names in Review 1 and completed review 2

--- src/gr11review/part1/Review1.java
+++ src/gr11review/part1/Review1.java
@@ -7,32 +7,37 @@ import java.io.*; // import code
 public class Review1{
     public static void main(String[] args) throws IOException{
         
+        // Create buffered reader object
         BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
 
         // Prompt user to input month and day number
         System.out.print("Enter the month number: ");
         int month = Integer.parseInt(keyboard.readLine());
 
-        System.out.print("Enter day number: ");
+        System.out.print("Enter the day number: ");
         int days = Integer.parseInt(keyboard.readLine());
 
         // Proccess the how many days are in each month and calculate the day of the year number
 
-        int daysOfYear = 0;
+        int daysOfMonths = 0; // Create variable to hold the days of months prior
+
+        // Proccess / Calculate the number of days prior to the month selected using while loop
         month -= 1;
 
         while (month > 0){
             if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12){
-                daysOfYear += 31;
+                daysOfMonths += 31;
             }
             else if(month == 4 || month == 6 || month == 9 || month == 11){
-                daysOfYear += 30;
+                daysOfMonths += 30;
             }
             else {
-                daysOfYear += 28;
+                daysOfMonths += 28;
             }
             month -= 1;
         }
-        System.out.println(daysOfYear + days);
+        
+        // Output total number of days by adding days prior to the month and days inputted.
+        System.out.println(daysOfMonths + days);
     }
 }
--- src/gr11review/part1/Review2.java
+++ src/gr11review/part1/Review2.java
@@ -1,13 +1,38 @@
 package gr11review.part1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 // Program to prompt user to choose a joke catergory and output corresponding joke
 
 public class Review2{
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         
+        // Create buffered reader object
+        BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
 
-
-
-
+        // Display menu and prompt user to select a menu option.
+        System.out.println("0 - print a joke about your hair\n1 - print a joke about your feet\n2 - print a joke about your clothes\n3 - print a joke about your teacher");
+        System.out.print("Choose a menu option: ");
+        int selectJoke = Integer.parseInt(keyboard.readLine());
+        
+        // Output joke depending on message selected. (Also Ive never used this before and had to look up how to do switch case)
+        switch (selectJoke) {
+        case 0:
+            System.out.println("What do you call a stew with hair in it?\nHairy Pot-ter");
+            break;
+        case 1:
+            System.out.println("What do you call an american foot?\nMissile-Toe");
+            break;
+        case 2:
+            System.out.println("What clothing do scammers wear?\nA Lawsuit");
+            break;
+        case 3:
+            System.out.println("What do you call a teacher who is stuck in traffic?\nLate");
+            break;
+        default:
+            System.out.print("Invalid menu option");
+        }
     }
 }
