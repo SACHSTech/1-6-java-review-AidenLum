0d5e2b784d5fc541fd1160176a0d522ded551d5e
AidenLum <aiden.lum25@ycdsbk12.ca>
2024-09-17 10:29:29 -0400

    WIP Completed review 7

--- src/gr11review/part1/Review6.java
+++ src/gr11review/part1/Review6.java
@@ -1,18 +1,19 @@
 package gr11review.part1;
 
-import java.io.*;
-import java.text.DecimalFormat;
+import java.io.*; // Import nativa java
+import java.text.DecimalFormat; // import decimal format
 
 
 public class Review6{
     public static void main(String[] args) throws IOException {
         
-        BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in)); // Create buffered reader object
         DecimalFormat formatter = new DecimalFormat("0.00"); // Create decimal format object
 
-        double dblPrice;
+        double dblPrice; // Create price and subototal variable
         double dblSubtotal = 0;
 
+        // Allow user to enter in price until 0 is entered and calculate subtotoal
         do { 
             System.out.print("Enter the price for an item: ");
             dblPrice = Double.parseDouble(keyboard.readLine());
@@ -20,8 +21,9 @@ public class Review6{
 
         } while (dblPrice > 0);
 
-        double dblTax = dblSubtotal * 0.13;
+        double dblTax = dblSubtotal * 0.13; // Calculate tax
 
+        // Output subtotal, tax, total
         System.out.println("Subtotal: $" + formatter.format(dblSubtotal));
         System.out.println("Tax: $" + formatter.format(dblTax));
         System.out.println("Total: $" + formatter.format(dblSubtotal + dblTax));
--- src/gr11review/part1/Review8.java
+++ src/gr11review/part1/Review8.java
@@ -1,1 +1,14 @@
 package gr11review.part1;
+
+public class Review8{
+    public static void main(String[] args) {
+        
+        int[] intRandom = new int[3];
+
+        for (int i = 0; i < 1000; i++){
+            for (int u = 1; u <= 4; u++){
+                int[i] = (Math.random() * 8);
+            }
+        }
+    }
+}
